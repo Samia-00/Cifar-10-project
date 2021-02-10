e8512038309f42d3f6e8b54c99d9b8054b5b8b58
Farjana Sultana Samia <73123239+Samia-00@users.noreply.github.com>
2021-02-10 08:50:15 +0600

    update

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ import os
 
 nb_train_samples = 50000
 nb_test_samples = 300000
+nb_classes = 10
 
 img_size = 32
 img_channel = 3
 img_shape =(img_size,img_size,img_channel)
+lr = 0.001
+batch_size = 200
+nb_epochs = 60
 
 def root_path():
     return os.path.dirname(__file__)
@@ -23,5 +27,8 @@ def src_path():
 
 def submission_path():
     return os.path.join(root_path(),"submission")
+
+def output_path():
+    return os.path.join(root_path(),"output")
 
 print(src_path())
